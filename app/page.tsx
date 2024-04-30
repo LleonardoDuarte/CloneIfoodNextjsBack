1cbc01ce004f4c68ba10cfe9c77e526532acd236
LeonardoDuarte <leopd_00@hotmail.com>
2024-04-30 18:16:00 -0300

    feat: add promo banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -9,7 +10,21 @@ const Home = () => {
       <div className="px-5 pt-6">
         <Search />
       </div>
-      <CategoryList />
+
+      <div className="px-5 py-6">
+        <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo01.png"
+          alt="Banner promocional até 30% de desconto em pizzas"
+          width={0}
+          height={0}
+          quality={100}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+      </div>
     </>
   );
 };
