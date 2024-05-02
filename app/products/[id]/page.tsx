bc70911ccd9907364ed0fb4afd66707168f48dbe
LeonardoDuarte <leopd_00@hotmail.com>
2024-05-01 21:32:47 -0300

    feat: card information about deliver time and deliver price

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,12 +1,7 @@
-import {
-  calculateProductTotalPrice,
-  formatCurrency,
-} from "@/app/_helpers/price";
 import { db } from "@/app/_lib/prisma";
-import Image from "next/image";
 import { notFound } from "next/navigation";
 import ProductImage from "../_components/product-image";
-import DiscountBadge from "@/app/_components/discount-badge";
+import ProductDetails from "../_components/product-details";
 
 interface ProductPageProps {
   params: {
@@ -32,39 +27,7 @@ const ProductPage = async ({ params: { id } }: ProductPageProps) => {
     <>
       <div>
         <ProductImage product={product} />
-        <div className="p-5">
-          <div className="flex items-center gap-[0.375rem]">
-            <div className="relative h-8 w-8">
-              <Image
-                src={product.restaurant.imageURL}
-                alt={product.restaurant.name}
-                fill
-                className="rounded-full object-cover"
-              />
-            </div>
-            <span className="text-xs text-muted-foreground">
-              {product.restaurant.name}
-            </span>
-          </div>
-          <h1 className="mb-2 mt-1 text-xl font-semibold">{product.name}</h1>
-
-          <div className="flex justify-between">
-            <div>
-              <div className="flex items-center gap-2">
-                <h2 className="px-1 text-xl font-semibold">
-                  {formatCurrency(calculateProductTotalPrice(product))}
-                </h2>
-
-                {product.discountPercentage > 0 && (
-                  <DiscountBadge product={product} />
-                )}
-              </div>
-              <span className="text-sl text-muted-foreground">
-                De: {formatCurrency(Number(product.price))}
-              </span>
-            </div>
-          </div>
-        </div>
+        <ProductDetails product={product} />
       </div>
     </>
   );
---
+++ app/products/_components/product-details.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import DiscountBadge from "@/app/_components/discount-badge";
+import { Button } from "@/app/_components/ui/button";
+import { Card } from "@/app/_components/ui/card";
+import {
+  calculateProductTotalPrice,
+  formatCurrency,
+} from "@/app/_helpers/price";
+import { Prisma } from "@prisma/client";
+import {
+  BikeIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  TimerIcon,
+} from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+
+interface ProductDetailsProps {
+  product: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: true;
+    };
+  }>;
+}
+
+const ProductDetails = ({ product }: ProductDetailsProps) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleIncreaseQuantityClick = () =>
+    setQuantity((currencyState) => currencyState + 1);
+
+  const handleDecreaseQuantityClick = () =>
+    setQuantity((currencyState) => {
+      if (currencyState === 1) return 1;
+
+      return currencyState - 1;
+    });
+
+  return (
+    <div className="p-5">
+      <div className="flex items-center gap-[0.375rem]">
+        <div className="relative h-8 w-8">
+          <Image
+            src={product.restaurant.imageURL}
+            alt={product.restaurant.name}
+            fill
+            className="rounded-full object-cover"
+          />
+        </div>
+        <span className="text-xs text-muted-foreground">
+          {product.restaurant.name}
+        </span>
+      </div>
+      <h1 className="mb-2 mt-1 text-xl font-semibold">{product.name}</h1>
+
+      <div className="flex justify-between">
+        <div>
+          <div className="flex items-center gap-2">
+            <h2 className="px-1 text-xl font-semibold">
+              {formatCurrency(calculateProductTotalPrice(product))}
+            </h2>
+
+            {product.discountPercentage > 0 && (
+              <DiscountBadge product={product} />
+            )}
+          </div>
+          {product.discountPercentage > 0 && (
+            <span className="text-sl text-muted-foreground">
+              De: {formatCurrency(Number(product.price))}
+            </span>
+          )}
+        </div>
+
+        <div className="flex items-center gap-3 text-center">
+          <Button
+            size="icon"
+            className="border-solid border-muted-foreground"
+            variant="ghost"
+            onClick={handleDecreaseQuantityClick}
+          >
+            <ChevronLeftIcon />
+          </Button>
+          <span className="w-4">{quantity}</span>
+          <Button size="icon" onClick={handleIncreaseQuantityClick}>
+            <ChevronRightIcon />
+          </Button>
+        </div>
+      </div>
+
+      <Card className="mt-6 flex justify-around py-6">
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-2 text-muted-foreground">
+            <span className="text-xs">Entrega</span>
+            <BikeIcon size={14} />
+          </div>
+
+          {Number(product.restaurant.deliverfee) > 0 ? (
+            <p className="text-xs font-semibold">
+              {formatCurrency(Number(product.restaurant.deliverfee))}
+            </p>
+          ) : (
+            <p className="text-sm font-semibold">Grátis</p>
+          )}
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-2 text-muted-foreground">
+            <span className="text-xs">Entrega</span>
+            <TimerIcon size={14} />
+          </div>
+          {product.restaurant.deliverTimeMinutes} min
+          {/* {Number(product.restaurant.deliverfee) > 0 ? (
+            <p className="text-xs font-semibold">
+              {formatCurrency(Number(product.restaurant.deliverfee))}
+            </p>
+          ) : (
+            <p className="text-sm font-semibold">Grátis</p>
+          )} */}
+        </div>
+      </Card>
+      <div className="mb-3 space-y-3">
+        <h3 className="font-semibold">Sobre</h3>
+        <p className="text-sm text-muted-foreground">{product.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
